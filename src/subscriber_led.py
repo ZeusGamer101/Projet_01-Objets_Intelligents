433546bb59780cc96f0974eaebc412e03df4629b
M7000-13 <mdouble2002@gmail.com>
2026-03-14 20:08:04 -0400

    Ajout de la section "Callback MQTT
    Ajout des fonctions suivantes:
    - on_connect
    - on_message
    - on_disconnect
    
    Ajout de bannière au dessus des sections pour mieux se retrouver dans le code

--- src/subscriber_led.py
+++ src/subscriber_led.py
@@ -3,7 +3,9 @@ from typing import Any
 import paho.mqtt.client as mqtt
 from gpiozero import LED
 
+#============================
 # Paramètres
+#============================
 BROKER_HOST = "localhost"
 BROKER_PORT = 1883
 KEEPALIVE_S = 60
@@ -21,6 +23,9 @@ TOPIC_STATE = f"ahuntsic/aec-iot/b3/{TEAM}/{DEVICE}/actuators/led/cmd"
 
 QOS_CMD = 1
 
+#============================
+# Fonctions utilitaires
+#============================
 def publish_led_state(client: mqtt.Client) -> None:
     state = "on" if led.is_lit else "off"
     client.publish(TOPIC_STATE, state, qos=1, retain=True)
@@ -45,3 +50,36 @@ def parse_command(payload_text: str) -> str | None:
             return "on"
         if v in (0, False, "0", "off", "OFF"):
             return "off"
+        
+#============================
+# Callback MQTT
+#============================
+def on_connect(client, userdata, flags, reason_code, properties=None):
+    print(f"[CONNECT] reason_code={reason_code}")
+    if reason_code == 0:
+
+        client.subscribe(TOPIC_CMD, qos=QOS_CMD)
+        print(f"[SUB] {TOPIC_CMD} (qos={QOS_CMD})")
+
+        
+        publish_led_state(client)
+        
+def on_message(client, userdata, msg: mqtt.MQTTMessage):
+    payload_text = msg.payload.decode("utf-8", errors="replace")
+    print(f"[MSG] topic={msg.topic} qos={msg.qos} retain={msg.retain} payload={payload_text}")
+    command = parse_command(payload_text)
+    if command is None:
+        print("[WARN] Commande invalide (JSON attendu). Ignorée.")
+        return
+    
+    # Action GPIO
+    if command == "on":
+        led.on()
+    else:
+        led.off()
+
+    
+    publish_led_state(client)
+def on_disconnect(client, userdata, reason_code, properties=None):
+    print(f"[DISCONNECT] reason_code={reason_code}")
+    led.off()
