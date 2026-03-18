574e5a387b5b1fd9b9bfd47ca87bca543fc4842c
Blondel <106287796+ZeusGamer101@users.noreply.github.com>
2026-03-18 16:02:00 -0400

    cote publisher demo temp pour l'instant terminer et tester

--- src/publisher_sensor.py
+++ src/publisher_sensor.py
@@ -58,3 +58,71 @@ def on_disconnect(client,userdata,reason_code,properties=None):
     global connected
     print(f"[DISCONNECT] reason_code={reason_code}")
     connected = False
+
+# ---------------------------------------------------------------------
+# 4) Cr�ation du client + LWT + connexion
+# ---------------------------------------------------------------------
+
+client = mqtt.Client(
+    client_id=CLIENT_ID,
+    protocol=mqtt.MQTTv311
+)
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+
+# LWT (Last Will): si le Pi meurt sans se d�connecter proprement,
+# le broker publiera "offline" sur TOPIC_ONLINE.
+
+client.will_set(
+    topic=TOPIC_ONLINE,
+    payload="offline",
+    qos=QOS_STATUS,
+    retain=True
+)
+
+# Option utile en IoT: d�lai de reconnexion progressif (�vite spam r�seau)
+# (On garde �a simple; on le renforcera dans les prochaines s�ances)
+client.reconnect_delay_set(min_delay=1,max_delay=30)
+
+# Connexion non bloquante + thread r�seau:
+client.connect_async(BROKER_HOST,BROKER_PORT,keepalive=KEEPALIVE_S)
+client.loop_start()
+
+# ---------------------------------------------------------------------
+# 5) Boucle principale (capteur -> publish)
+# ---------------------------------------------------------------------
+try:
+    client.publish(TOPIC_ONLINE,"online",qos=QOS_STATUS,retain=True)
+
+    while True:
+        if not connected:
+            print("[WAIT] en attente de connexion MQTT...")
+            time.sleep(1.0)
+            continue
+    
+        temperature_c = read_temperature_c()
+
+        payload = {
+            "device_id": DEVICE,
+            "sensor" : "temperature",
+            "value" : temperature_c,
+            "unit" : "C",
+            "ts" : datetime.now(timezone.utc).isoformat()
+        }
+
+        # 1) Message JSON (contrat "riche")
+        client.publish(TOPIC_JSON,json.dumps(payload),qos=QOS_SENSOR,retain=False)
+
+        # 2) Valeur simple (facile pour dashboards)
+        client.publish(TOPIC_VALUE,str(temperature_c),qos=QOS_SENSOR,retain=False)
+
+        print(f"[PUB] {TOPIC_JSON} -> {payload}")
+        time.sleep(PUBLISH_PERIOD_S)
+
+except KeyboardInterrupt:
+    print("\n[STOP] arr�t demand� (Ctrl+C)")
+finally:
+    client.publish(TOPIC_ONLINE,"offline",qos=QOS_STATUS,retain=True)
+    client.loop_stop()
+    client.disconnect()
